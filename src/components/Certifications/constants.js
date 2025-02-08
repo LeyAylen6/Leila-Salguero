22bb05917eb24d6a4aab307a00256b4632fa47b3
Leila <leiisalguero@gmaill.com>
2025-02-07 21:22:20 -0300

    fix

--- src/components/Certifications/constants.js
+++ src/components/Certifications/constants.js
@@ -1,13 +1,13 @@
 
 import styles from './certifications.module.css';
 
-import AdvancedJava from '../../assets/certifications/AdvancedJava.jpeg'
 import Java from '../../assets/certifications/Java.jpeg';
 import Testing from '../../assets/certifications/Testing.jpeg';
 import FullStack from '../../assets/certifications/Fullstack.jpeg'
 import SpringBoot from '../../assets/certifications/SpringBoot.jpeg'
 import Python from '../../assets/certifications/Python.jpeg'
 import Scrum from '../../assets/certifications/Scrum.png'
+import AdvancedJava from '../../assets/certifications/AdvancedJava.jpeg'
 
 export const certifications = [
     {
